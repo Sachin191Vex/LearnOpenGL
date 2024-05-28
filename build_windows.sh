cb0115345e691877b4e8da220399f13babb83b10
Sachin191vex <sachin191vex@gmail.com>
2024-05-27 21:23:24 -0700

    Chnaged Build script to build with VS2022

--- build_windows.sh
+++ build_windows.sh
@@ -1,5 +1,5 @@
 mkdir -p out
 cd out
 
-cmake -G"Visual Studio 16 2019" ${COMMON_CMAKE_CONFIG_PARAMS} ../
+cmake -G"Visual Studio 17 2022" ${COMMON_CMAKE_CONFIG_PARAMS} ../
 cmake --build . --config Debug
